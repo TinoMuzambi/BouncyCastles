c8525b153f87cb846ba95b408a92c79dce579be1
Tino Muzambi <tinomuzambi@gmail.com>
2022-05-22 17:12:43 +0200

    Fixes enceyption.

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,33 +1,35 @@
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.Strings;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
 import java.security.GeneralSecurityException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.Security;
-import java.security.spec.RSAKeyGenParameterSpec;
 import java.util.Arrays;
 
 public class Encryption {
-    public static KeyPair generateKeyPair() throws GeneralSecurityException {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
-        keyPairGenerator.initialize(new RSAKeyGenParameterSpec(20, RSAKeyGenParameterSpec.F4));
-        return keyPairGenerator.generateKeyPair();
+    public static SecretKey generateKey()
+            throws GeneralSecurityException
+    {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES", "BC");
+        keyGenerator.init(256);
+        return keyGenerator.generateKey();
     }
 
     public static byte[][] cbcEncrypt(SecretKey key, byte[] data)
             throws GeneralSecurityException {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
         cipher.init(Cipher.ENCRYPT_MODE, key);
         return new byte[][]{cipher.getIV(), cipher.doFinal(data)};
     }
 
     public static byte[] cbcDecrypt(SecretKey key, byte[] iv, byte[] cipherText)
             throws GeneralSecurityException {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
         cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
         return cipher.doFinal(cipherText);
     }
@@ -46,50 +48,61 @@ public class Encryption {
      * @return The flattened 1D array
      */
     public static byte[] flatten2DArray(byte[][] array) {
-
         byte[] flattenedArray = new byte[array.length * array[0].length];
         for (int i = 0; i < array.length; i++) {
             System.arraycopy(array[i], 0, flattenedArray, i * array[0].length, array[0].length);
         }
         return flattenedArray;
+    }
 
+    /**
+     * Install BC Fips provider.
+     */
+    public static void installProvider()
+    {
+        Security.addProvider(new BouncyCastleProvider());
     }
 
     public static void main(String[] args) throws GeneralSecurityException {
-        // Add BouncyCastleProvider.
-        Security.addProvider(new BouncyCastleProvider());
+        installProvider();
 
-        // Initialise sender/receiver keys.
-        KeyPair anneKeys;
-        KeyPair bobKeys;
+        defineKey(new byte[128 / 8]);
+        defineKey(new byte[192 / 8]);
+        defineKey(new byte[256 / 8]);
 
-        // Generation of sender/receiver public and private keys.
-        anneKeys = generateKeyPair();
-        System.out.println("Anne's public key - " + anneKeys.getPublic());
-        System.out.println("Anne's private key - " + anneKeys.getPrivate());
+        // Initialise and generation of sender/receiver keys.
+        SecretKey annePublicKey = generateKey();
+        defineKey(new byte[128 / 8]);
+        defineKey(new byte[192 / 8]);
+        defineKey(new byte[256 / 8]);
+        SecretKey annePrivateKey = generateKey();
+        defineKey(new byte[128 / 8]);
+        defineKey(new byte[192 / 8]);
+        defineKey(new byte[256 / 8]);
+        SecretKey bobPublicKey = generateKey();
+        defineKey(new byte[128 / 8]);
+        defineKey(new byte[192 / 8]);
+        defineKey(new byte[256 / 8]);
+        SecretKey bobPrivateKey = generateKey();
 
-        bobKeys = generateKeyPair();
-        System.out.println("Bob's public key - " + bobKeys.getPublic());
-        System.out.println("Bob's private key - " + bobKeys.getPrivate());
+        System.out.println("Anne's public key - " + annePublicKey);
+        System.out.println("Anne's private key - " + annePrivateKey);
+
+        System.out.println("Bob's public key - " + annePublicKey);
+        System.out.println("Bob's private key - " + bobPublicKey);
 
         // The below represents encryption. This provides us with message authentication.
         // Generate Bob's message.
-        byte[] bobMsg = new byte[4];
-        bobMsg[0] = 4;
-        bobMsg[1] = 16;
-        bobMsg[2] = 8;
-        bobMsg[3] = 0;
-        System.out.println("Bob's unencrypted message - " + Arrays.toString(bobMsg));
+        byte[] bobMsg = Strings.toByteArray("Houston, we have a landing");
+        System.out.println("Bob's plaintext message - " + Arrays.toString(bobMsg));
 
         // Encrypt Bob's message with Anne's public key.
-        byte[][] bobEncryptedMsg = cbcEncrypt(defineKey(anneKeys.getPublic().getEncoded()), bobMsg);
-        System.out.println("Bob's encrypted message - " + Arrays.toString(flatten2DArray(bobEncryptedMsg)));
+        byte[][] bobEncryptedMsg = cbcEncrypt(annePublicKey, bobMsg);
+        System.out.println("Bob's encrypted message - " + Arrays.toString(bobEncryptedMsg[1]));
 
         // Decrypt Bob's message with Anne's private key.
-        byte[] iv = new byte[2];
-        iv[0] = 20;
-        iv[1] = 4;
-        byte[] bobDecryptedMsg = cbcDecrypt(defineKey(anneKeys.getPrivate().getEncoded()), iv, flatten2DArray(bobEncryptedMsg));
+        // TODO investigate keys.
+        byte[] bobDecryptedMsg = cbcDecrypt(annePublicKey, bobEncryptedMsg[0], bobEncryptedMsg[1]);
         System.out.println("Bob's decrypted message - " + Arrays.toString(bobDecryptedMsg));
     }
 }
