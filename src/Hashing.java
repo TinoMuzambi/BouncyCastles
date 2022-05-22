d79caae15f128d3172e5ffcefbb0f30442493d23
Tino Muzambi <tinomuzambi@gmail.com>
2022-05-22 13:43:07 +0200

    Throws exception.

--- src/Hashing.java
+++ src/Hashing.java
@@ -1,4 +1,5 @@
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import java.security.*;
 import java.security.spec.RSAKeyGenParameterSpec;
 import java.util.Arrays;
@@ -11,57 +12,54 @@ public class Hashing {
     }
 
     public static byte[] generateX931Signature(PrivateKey rsaPrivate, byte[] input)
-            throws GeneralSecurityException
-    {
+            throws GeneralSecurityException {
         Signature signature = Signature.getInstance("SHA384withRSA/X9.31", "BC");
         signature.initSign(rsaPrivate);
         signature.update(input);
         return signature.sign();
     }
+
     public static boolean verifyX931Signature(PublicKey rsaPublic, byte[] input, byte[] encSignature)
-            throws GeneralSecurityException
-    {
+            throws GeneralSecurityException {
         Signature signature = Signature.getInstance("SHA384withRSA/X9.31", "BC");
         signature.initVerify(rsaPublic);
         signature.update(input);
         return signature.verify(encSignature);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws GeneralSecurityException {
         // Add BouncyCastleProvider.
         Security.addProvider(new BouncyCastleProvider());
 
         // Initialise sender/receiver keys.
         KeyPair anneKeys;
         KeyPair bobKeys;
-        try {
-            // Generation of sender/receiver public and private keys.
-            anneKeys = generateKeyPair();
-            System.out.println("Anne's public key - " + anneKeys.getPublic());
-            System.out.println("Anne's private key - " + anneKeys.getPrivate());
 
-            bobKeys = generateKeyPair();
-            System.out.println("Bob's public key - " + bobKeys.getPublic());
-            System.out.println("Bob's private key - " + bobKeys.getPrivate());
+        // Generation of sender/receiver public and private keys.
+        anneKeys = generateKeyPair();
+        System.out.println("Anne's public key - " + anneKeys.getPublic());
+        System.out.println("Anne's private key - " + anneKeys.getPrivate());
 
-            // The below represents hashing. This provides us with message integrity
-            // Generate Anne's message.
-            byte[] anneMsg = new byte[4];
-            anneMsg[0]=20;
-            anneMsg[1]=10;
-            anneMsg[2]=5;
-            anneMsg[3]=30;
-            System.out.println("Anne's unsigned message - " + Arrays.toString(anneMsg));
+        bobKeys = generateKeyPair();
+        System.out.println("Bob's public key - " + bobKeys.getPublic());
+        System.out.println("Bob's private key - " + bobKeys.getPrivate());
 
-            // Sign Anne's message with Anne's private key.
-            byte[] anneSignedMsg = generateX931Signature(anneKeys.getPrivate(), anneMsg);
-            System.out.println("Anne's signed message - " + Arrays.toString(anneSignedMsg));
+        // The below represents hashing. This provides us with message integrity
+        // Generate Anne's message.
+        byte[] anneMsg = new byte[4];
+        anneMsg[0] = 20;
+        anneMsg[1] = 10;
+        anneMsg[2] = 5;
+        anneMsg[3] = 30;
+        System.out.println("Anne's unsigned message - " + Arrays.toString(anneMsg));
 
-            // Verify Anne's message with Anne's public key.
-            boolean bobReceivesAnneMsg = verifyX931Signature(anneKeys.getPublic(), anneMsg, anneSignedMsg);
-            System.out.println("Bob successfully verified Anne's message - " + bobReceivesAnneMsg);
-        } catch (GeneralSecurityException e) {
-            System.out.println(e);
-        }
+        // Sign Anne's message with Anne's private key.
+        byte[] anneSignedMsg = generateX931Signature(anneKeys.getPrivate(), anneMsg);
+        System.out.println("Anne's signed message - " + Arrays.toString(anneSignedMsg));
+
+        // Verify Anne's message with Anne's public key.
+        boolean bobReceivesAnneMsg = verifyX931Signature(anneKeys.getPublic(), anneMsg, anneSignedMsg);
+        System.out.println("Bob successfully verified Anne's message - " + bobReceivesAnneMsg);
+
     }
 }
