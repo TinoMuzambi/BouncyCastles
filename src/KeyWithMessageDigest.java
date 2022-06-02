222246d003f72e8495dcfa86ecbd027f34dc4d6b
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-02 13:39:59 +0200

    Adds class for wrapping key with message digest.

---
+++ src/KeyWithMessageDigest.java
@@ -0,0 +1,18 @@
+public class KeyWithMessageDigest {
+
+    private final byte[][] oneTimeKey;
+    private final byte[][] messageDigest;
+
+    public KeyWithMessageDigest(byte[][] oneTimeKey, byte[][] messageDigest) {
+        this.oneTimeKey = oneTimeKey;
+        this.messageDigest = messageDigest;
+    }
+
+    public byte[][] getOneTimeKey() {
+        return oneTimeKey;
+    }
+
+    public byte[][] getMessageDigest() {
+        return messageDigest;
+    }
+}
