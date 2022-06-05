0f40f8dd80f709485b082935b708bfb649bb4d78
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-05 22:58:17 +0200

    Adds sever key pair.

--- src/Server.java
+++ src/Server.java
@@ -1,45 +1,47 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class Server {
 
     private ServerSocket serverSocket;
+    KeyPair pair = Hashing.generateKeyPair();
+    private final PrivateKey privateKey = pair.getPrivate();
+    private final PublicKey publicKey = pair.getPublic();
 
-    public Server(ServerSocket serverSocket) {
+    public Server(ServerSocket serverSocket) throws GeneralSecurityException {
         this.serverSocket = serverSocket;
     }
 
-    public void startServer(){
+    public void startServer() throws IOException {
         System.err.println("SERVER STARTED");
 
-        try{
+        while (!serverSocket.isClosed()) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Connection established!");
+            ClientHandler clientHandler = new ClientHandler(socket);
 
-            while (!serverSocket.isClosed()) {
-                Socket socket = serverSocket.accept();
-                System.out.println("Connection established!");
-                ClientHandler clientHandler = new ClientHandler(socket);
-
-                Thread thread = new Thread(clientHandler);
-                thread.start();
-
-
-            }
-        } catch (IOException e){
-
+            Thread thread = new Thread(clientHandler);
+            thread.start();
         }
-
     }
-    public void closeServerSocket(){
-        try{
-            if (serverSocket != null){
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
                 serverSocket.close();
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        Hashing.installProvider();
         ServerSocket serverSocket = new ServerSocket(1235);
         Server server = new Server(serverSocket);
         server.startServer();
