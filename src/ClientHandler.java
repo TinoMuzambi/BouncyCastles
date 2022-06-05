0d39c435adee54dbba623d80d11fa6cc893c9fad
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-05 22:49:57 +0200

    Revert code to initial state.

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,14 +1,7 @@
 import java.io.*;
 import java.net.Socket;
-import java.security.KeyFactory;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Base64;
+import java.util.logging.SocketHandler;
 
 public class ClientHandler implements Runnable{
 
@@ -17,27 +10,22 @@ public class ClientHandler implements Runnable{
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String name;
-    private PublicKey publicKey;
-    private PublicKey serverPublicKey;
+    private String message;
 
 
-    public ClientHandler(Socket socket, PublicKey serverPublicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public ClientHandler(Socket socket){
         try {
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.name = bufferedReader.readLine();
-//            String nameWithPublicKey = bufferedReader.readLine();
-//            System.out.println("ch - " + nameWithPublicKey);
-//            this.name = nameWithPublicKey.split(" - ")[0];
-//            byte[] publicBytes = Base64.getDecoder().decode(nameWithPublicKey.split(" - ")[1]);
-//            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
-//            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-//            publicKey = keyFactory.generatePublic(keySpec);
-//            this.serverPublicKey = serverPublicKey;
+
 
             clientHandlers.add(this);
-            broadcastMessage("SERVER: " + name + " has joined the group chat");
+            broadcastMessage("SERVER: " + name +"" +" " + " has joined the group chat");
+
+
+
         } catch (IOException e){
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
@@ -56,6 +44,7 @@ public class ClientHandler implements Runnable{
                 break;
             }
         }
+
     }
 
     public void broadcastMessage(String messageToSend){
@@ -65,22 +54,19 @@ public class ClientHandler implements Runnable{
                     clientHandler.bufferedWriter.write(messageToSend);
                     clientHandler.bufferedWriter.newLine();
                     clientHandler.bufferedWriter.flush();
-                } else {
-                    clientHandler.bufferedWriter.write("UK:SERVER: " + Arrays.toString(serverPublicKey.getEncoded()));
-                    clientHandler.bufferedWriter.newLine();
-                    clientHandler.bufferedWriter.flush();
+
                 }
+
             } catch (IOException e){
                 closeEverything(socket, bufferedReader, bufferedWriter);
             }
         }
     }
-
     public void removeClientHandler(){
         clientHandlers.remove(this);
-        broadcastMessage("SERVER: " + name +" " + "has left the chat!");
-    }
+        broadcastMessage("SERVER: " + name +"" +" " + "has left the chat!");
 
+    }
     public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
         removeClientHandler();
         try {
@@ -96,5 +82,7 @@ public class ClientHandler implements Runnable{
         } catch (IOException e){
             e.printStackTrace();
         }
+
     }
+
 }
--- src/Server.java
+++ src/Server.java
@@ -1,39 +1,48 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.*;
-import java.security.spec.InvalidKeySpecException;
 
 public class Server {
-    private final ServerSocket serverSocket;
-    KeyPair pair = Hashing.generateKeyPair();
-    private final PrivateKey privateKey = pair.getPrivate();
-    private final PublicKey publicKey = pair.getPublic();
 
-    public Server(ServerSocket serverSocket) throws GeneralSecurityException {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
     }
 
     public void startServer(){
+
         try{
+
             while (!serverSocket.isClosed()) {
-                System.err.println("SERVER STARTED");
                 Socket socket = serverSocket.accept();
                 System.out.println("Connection established!");
-                ClientHandler clientHandler = new ClientHandler(socket, publicKey);
+                ClientHandler clientHandler = new ClientHandler(socket);
 
                 Thread thread = new Thread(clientHandler);
                 thread.start();
+
+
             }
-        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException ignored){
+        } catch (IOException e){
+
+        }
+
+    }
+    public void closeServerSocket(){
+        try{
+            if (serverSocket != null){
+                serverSocket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
-        Hashing.installProvider();
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(1235);
         Server server = new Server(serverSocket);
         server.startServer();
+
     }
 }
 
