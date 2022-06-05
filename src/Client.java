b37e1d002bf4e7cb77e99b619ed5449dbe3a09a4
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-05 23:12:25 +0200

    Problem is with generating client's key pair.

--- src/Client.java
+++ src/Client.java
@@ -14,58 +14,51 @@ public class Client {
     private BufferedWriter bufferedWriter;
     private String name;
     private String message;
-
     private PrivateKey privateKey;
     private PublicKey publicKey;
-    private static final String PRIVATE_KEY_STRING = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJ2qoa4HT8GQcxohvKte2YJMUpANDKQXevxBpFVSWfinX/JGP3ZPzTE2bmcJokByq3nRYGvZe3sqBGaaTfTuECvG/9H0izCbT4ESWyg4h6bXwjkJqZs75XgZP93zby/aD+SeYveCp4nAmLRJSoCStarSAnIyNPkFrq5CBQnQNAdnAgMBAAECgYAFeM8MradIdf1wqjwUjIGrAoAZDMLkDQaRK6wK5AVIWnK7g1Gfhwx9iUCSpeRyLSijXS9l/tHIyAAIGZHcDskT1dpkOgOWqnRrotnoUFPAWojdxJ2CUR8DCve4dC0f/RWFLK1FmPfoqpbe0GcWjnW4l9rZbRft4+YD+ZGGJ86YbQJBALULb/Uqpne5Upz8B0UjYVefrh+ENMgQmNHeK9GHdCsOukbbc8rBXW+cjAOqfT1ncipM3gHA1uNoTOFfbIgHBx0CQQDe8WQljZi1q3GQt4UYhs3+gZ184gubsbzIrKoYuSybMcJD5SCIZr+0I6yL3l8dOSsHGpAWagFmjGbwfSBsnU1TAkEAq5J3O4R71iJO5G8EBMWOzpeJUFzeGGuCKAHAzQTtVpDSf6whhBjUD53wItlDbxnF28iU7FxwSXriwQLJu+/NEQJABRsoPQfyMG79yd+6J6amvyZ+0eZnd6hpd3sk5i4PjHpmRaHQze70gw6yG/361bOCfcYo+Kpy38NldCqZfINZywJARqpKZwDFcksA7h+SqiS6xG+Szo9BiaZ1vQ7B8lcQFQl/bcWlMuxRO7yFrex/0L3zrOgBlX0khk8FCQnA19dDYg==";
-    private static final String PUBLIC_KEY_STRING = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdqqGuB0/BkHMaIbyrXtmCTFKQDQykF3r8QaRVUln4p1/yRj92T80xNm5nCaJAcqt50WBr2Xt7KgRmmk307hArxv/R9Iswm0+BElsoOIem18I5CambO+V4GT/d828v2g/knmL3gqeJwJi0SUqAkrWq0gJyMjT5Ba6uQgUJ0DQHZwIDAQAB";
 
 
-    public Client(Socket socket, String name, String message){
-
+    public Client(Socket socket, String name, String message) throws GeneralSecurityException {
         try {
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.name = name;
-
-
-
+            this.message = message;
+            KeyPair pair = Hashing.generateKeyPair();
+            PublicKey UK = pair.getPublic();
+            PrivateKey RK = pair.getPrivate();
+            init(UK, RK);
+//            this.bufferedWriter.write(name + " - " + Arrays.toString(publicKey.getEncoded()));
         } catch (IOException e){
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
-    public Client(){
+    public Client() throws GeneralSecurityException {
 
 
     }
 
-    public void init(){
-        try{
-            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-            generator.initialize(1024);
-            KeyPair pair = generator.generateKeyPair();
-            privateKey = pair.getPrivate();
-            publicKey = pair.getPublic();
-        }catch (Exception ignored){
-        }
+    public void init(PublicKey UK, PrivateKey RK){
+            privateKey = RK;
+            publicKey = UK;
     }
 
-    public void initFromStrings(){
-        try{
-            X509EncodedKeySpec keySpecPublic = new X509EncodedKeySpec(decode(PUBLIC_KEY_STRING));
-            PKCS8EncodedKeySpec keySpecPrivate = new PKCS8EncodedKeySpec(decode(PRIVATE_KEY_STRING));
-
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-
-            publicKey = keyFactory.generatePublic(keySpecPublic);
-            privateKey = keyFactory.generatePrivate(keySpecPrivate);
-
-
-        }catch (Exception ignored){}
-
-
-    }
+//    public void initFromStrings(){
+//        try{
+//            X509EncodedKeySpec keySpecPublic = new X509EncodedKeySpec(decode(PUBLIC_KEY_STRING));
+//            PKCS8EncodedKeySpec keySpecPrivate = new PKCS8EncodedKeySpec(decode(PRIVATE_KEY_STRING));
+//
+//            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+//
+//            publicKey = keyFactory.generatePublic(keySpecPublic);
+//            privateKey = keyFactory.generatePrivate(keySpecPrivate);
+//
+//
+//        }catch (Exception ignored){}
+//
+//
+//    }
 
     public void setPrivateKey(){
         System.err.println("Public key\n"+ encode(publicKey.getEncoded()));
@@ -94,9 +87,6 @@ public class Client {
     public void sendMessage(){
         try {
             bufferedWriter.write(name);
-
-
-
             bufferedWriter.newLine();
             bufferedWriter.flush();
 
@@ -104,9 +94,6 @@ public class Client {
             while (socket.isConnected()){
                 String messageToSend = scanner.nextLine();
                 bufferedWriter.write(name + ":" + messageToSend);
-
-
-
                 bufferedWriter.newLine();
                 bufferedWriter.flush();
             }
@@ -114,8 +101,6 @@ public class Client {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
-
-
 
     public void listenForMessage(){
         new Thread(new Runnable() {
@@ -165,12 +150,9 @@ public class Client {
             System.out.println("Enter your message");
             String message = scanner.nextLine();
 
-
-
             Socket socket = new Socket("localhost", 1235);
 
             Client client = new Client(socket, name, message);
-            client.initFromStrings();
 
             String encryptedMessage = client.encrypt(message);
             String descruptedMessage = client.decrypt(encryptedMessage);
@@ -181,9 +163,7 @@ public class Client {
             client.listenForMessage();
             client.sendMessage();
 
-        }catch (Exception ignored){}
-
+        } catch (Exception ignored){}
     }
-
 
 }
