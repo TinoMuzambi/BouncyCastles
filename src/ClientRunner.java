e86c5da5ea5953c6e7eeb17fa63e32ef8e83c51b
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-07 12:56:54 +0200

    Sets multi use true.

--- src/ClientRunner.java
+++ src/ClientRunner.java
@@ -4,6 +4,7 @@ import java.util.Base64;
 
 public class ClientRunner {
     public static void main(String[] args) throws GeneralSecurityException {
+        System.setProperty("org.bouncycastle.rsa.allow_multi_use", "true");
         Hashing.installProvider();
         KeyPair pair = Hashing.generateKeyPair();
         String[] keys = {Base64.getEncoder().encodeToString(pair.getPrivate().getEncoded()), Base64.getEncoder().encodeToString(pair.getPublic().getEncoded())};
